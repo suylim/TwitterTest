fd859d37d12d2707acdc96165f1670a9038adb9d
suylim <suyashlimaye@gmail.com>
2019-05-13 23:32:47 +0530

    Initial commit

---
+++ src/main/java/com/home/SparkStream.java
@@ -0,0 +1,62 @@
+package com.home;
+
+import java.util.Arrays;
+import java.util.regex.Pattern;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.streaming.Durations;
+import org.apache.spark.streaming.api.java.JavaDStream;
+import org.apache.spark.streaming.api.java.JavaPairDStream;
+import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
+import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import org.apache.spark.streaming.twitter.*;
+
+import scala.Tuple2;
+import twitter4j.Status;
+
+public class SparkStream {
+    public static final Pattern SPACE =Pattern.compile(" ");
+    public static void main(String[] args)  {
+
+
+        String host="localhost";
+        int port = 9999;
+
+        SparkConf sparkConf = new SparkConf().setMaster("local").setAppName("JavaTwitterHashTagJoinSentiments");
+
+
+        String consumerKey = "Wswc51HXfGwepKueRd9KpHpJK";
+        String consumerSecret = "qcldScTBqbO0QgvxyQkN4RdBfTNkCyQF12x74FMOx0QFwxA9XO";
+        String accessToken = "98895636-LrWNYaE2nGtb4HqIEl5ng6hqaDUNb0kyYzNE8Eo19";
+        String accessTokenSecret = "250IcebcJPqAkhs0rPLVLavSbTGXhsNTA5uesQ0jyvKWZ";
+        String[] filters = {"hadoop"};
+
+        System.setProperty("twitter4j.oauth.consumerKey", consumerKey);
+        System.setProperty("twitter4j.oauth.consumerSecret", consumerSecret);
+        System.setProperty("twitter4j.oauth.accessToken", accessToken);
+        System.setProperty("twitter4j.oauth.accessTokenSecret", accessTokenSecret);
+
+        JavaStreamingContext jsc = new JavaStreamingContext(sparkConf,Durations.seconds(1));
+        JavaReceiverInputDStream<Status> lines = TwitterUtils.createStream(jsc,filters);
+        jsc.checkpoint("/tmp/checkpoint_nw");
+
+        //JavaReceiverInputDStream<String> lines= jsc.socketTextStream(host, port,StorageLevels.MEMORY_AND_DISK_SER);
+
+        JavaDStream<String> words= lines.flatMap(x->Arrays.asList(x.getText().split(" ")).iterator());
+
+        JavaPairDStream<String, Integer> wordcounts= words.mapToPair(x->new Tuple2<String, Integer>(x,1));
+
+        JavaPairDStream<String, Integer> counts=wordcounts.reduceByKey((x1,x2)->x1+x2);
+
+        counts.checkpoint(Durations.seconds(2));
+
+        counts.print();
+
+        jsc.start();
+
+        jsc.awaitTermination();
+
+
+    }
+
+}
